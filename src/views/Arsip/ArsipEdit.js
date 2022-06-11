fb80ccb43e58b365d2cf43b828288510cb3ba9c0
= <=>
2022-06-11 16:08:20 +0700

    2

--- src/views/Arsip/ArsipEdit.js
+++ src/views/Arsip/ArsipEdit.js
@@ -19,8 +19,7 @@ function ArsipEdit() {
     const updateAPIData = () => {
         apiClient.put('http://localhost:8000/api/arsip/${id}', {
             nama_arsip, keterangan, file_arsip
-        }).then(() => {
-            history.push
+        
         })
     }
 
